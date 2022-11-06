56be1908e8f228bfc95718b01a9102a27749bb7f
Younghwan <dudghks021121@gmail.com>
2022-11-06 20:57:45 +0900

    Feat; main.js에서 sendFile로 html파일과 css 파일 연결

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-import express, { application } from 'express';
+import express from 'express';
+import path from 'path';
 
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("fuck");
-})
+const __dirname = path.resolve();
+
+app.set('viewengine', 'html');
+app.use(express.static("public"));
+
+app.get('/main', (req,res)=>{
+    res.sendFile(__dirname+'/views/main.html');
+});
 
 app.listen(port);
